22b8723cdeaf9011df1afa27f96ac0174e1eb1ab
Helber Palheta <hpalheta@gmail.com>
2022-09-22 08:37:52 -0300

    Update mp_amazonforest.R
    Adjusts comments

--- amazonforest/ic/Modeling/rscript/mp_amazonforest.R
+++ amazonforest/ic/Modeling/rscript/mp_amazonforest.R
@@ -1,6 +1,23 @@
 #!/usr/bin/env Rscript
 
-#make_predictions_from_py.R T N N N N P B T
+#' 
+#' This is the R Script to perform AmazonForest for functional impact of variant genetics.
+#' 
+#' The AmazonForest model is a random forest based model trained and tested with eight single 
+#' functional predictors
+#' 
+#' The model is described in https://www.mdpi.com/2079-7737/11/4/538.
+#' 
+#' Citation: Palheta, H.G.A.; Gonçalves, W.G.; Brito, L.M.; dos Santos, A.R.; dos Reis Matsumoto, M.; 
+#'           Ribeiro-dos-Santos, Â.; de Araújo, G.S. AmazonForest: In Silico Metaprediction of Pathogenic 
+#'           Variants. Biology 2022, 11, 538. https://doi.org/10.3390/biology11040538
+#' 
+#' Corresponding authors: Prof. Dr. Gilderlanio Santana de Araujo (gilderlanio [at] gmail.com)
+#'                        Ms.C Helber Palheta (hpalheta [at] gmail.com)
+
+
+#' 0) Required libraries.
+
 suppressPackageStartupMessages(library("ROCR"))
 suppressPackageStartupMessages(library("randomForest"))
 suppressPackageStartupMessages(library("caret"))
@@ -17,16 +34,6 @@ SIFT <- args[8]
 pathdata <- args[9]
 pathmodel <- args[10]
 
-# FATHMM <- 'D'
-# LRT_pred <- 'N'
-# MutaAss <- 'L'
-# MutaTaster <- 'N'
-# PROVEAN <- 'D'
-# Pph2_HDIV <- 'D'
-# Pph2_HVAR <- 'P'
-# SIFT <- 'D'
-# pathdata <- '/Users/helberpalheta/workhp/DesenvolvimentoDoutorado/Doutorado/AmazonForest/AmazonForest/amazonforest/ic/Modeling/rscript/data/pdclinvar.ori.train.csv'
-# pathmodel <- '/Users/helberpalheta/workhp/DesenvolvimentoDoutorado/Doutorado/AmazonForest/AmazonForest/amazonforest/ic/Modeling/rscript/data/RF13.Rdata'
 
 # Load data to perform new predictions.
 predict_values <- c(FATHMM,LRT_pred,MutaAss,MutaTaster,PROVEAN,Pph2_HDIV,Pph2_HVAR,SIFT)
